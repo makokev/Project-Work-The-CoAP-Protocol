5ff4b6174b0758ca892923c7452a2042dff6e441
KevinLeto <kevinleto1995@gmail.com>
2019-01-17 17:22:22 +0100

    First version of client-server coap system. It doesn't use CoAP jet. This is only a initial system schema.

---
+++ coapRadar-v0/srcMore/it/unibo/ctxCoapSystemV0/coapsystemv0.pl
@@ -0,0 +1,12 @@
+%====================================================================================
+% Context ctxCoapSystemV0  SYSTEM-configuration: file it.unibo.ctxCoapSystemV0.coapSystemV0.pl 
+%====================================================================================
+context(ctxcoapsystemv0, "localhost",  "TCP", "8000" ).  		 
+%%% -------------------------------------------
+qactor( coap_server , ctxcoapsystemv0, "it.unibo.coap_server.MsgHandle_Coap_server"   ). %%store msgs 
+qactor( coap_server_ctrl , ctxcoapsystemv0, "it.unibo.coap_server.Coap_server"   ). %%control-driven 
+qactor( coap_client , ctxcoapsystemv0, "it.unibo.coap_client.MsgHandle_Coap_client"   ). %%store msgs 
+qactor( coap_client_ctrl , ctxcoapsystemv0, "it.unibo.coap_client.Coap_client"   ). %%control-driven 
+%%% -------------------------------------------
+%%% -------------------------------------------
+
